a91a3f9bd8b65fc4dfa25f29b8ebc05fbf0901c1
Killercat <rayershih@gmail.com>
2020-05-10 23:12:52 +0800

    Responder implementation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class Chatter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.receiveResponse = this.receiveResponse.bind(this);
+    }
+
+    receiveResponse(prompt, next) {
+        this.props.onReceivedMessage(prompt + " --- " + next);
+    }
+
+    submit = () => {
+        //let url = "http://localhost:8080/chatbot"
+        let url = "http://node.rayer.idv.tw:8800/chatbot"
+        let user = document.getElementById("chatbot_n").value
+        let input = document.getElementById("chatbot_p").value
+        console.log("submitted : " + user + " " + input);
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            redirect: "follow",
+            body: JSON.stringify({"user": user, "input": input})
+        }).then((response) => {
+            return response.json()
+        }).then(result => {
+            console.log(result);
+            this.receiveResponse(result.message, result.next)
+        });
+    }
+
+    render() {
+        return <form action="http://node.rayer.idv.tw:8800/chatbot" method="post">
+            <fieldset>
+                <legend>ChatBot test client</legend>
+                Name : <label htmlFor="chatbot_n"/><input type="text" name="user" id="chatbot_n"/><br/>
+                Chat : <label htmlFor="chatbot_p"/><input type="text" name="input" id="chatbot_p"/><br/>
+                {/*<input type="submit" value="submit!" id="chatbot_submit" onClick={this.submit}/>*/}
+                <button type="button" onClick={this.submit.bind()}>Submit!</button>
+            </fieldset>
+        </form>;
+    }
 }
+
+class Responder extends React.Component {
+    constructor(prop) {
+        super(prop);
+    }
+    render() {
+        if(this.props.willRenderMessage.length === 0) {
+            return <h1> I am RESPONSER!</h1>
+        } else {
+            return <h2> {this.props.willRenderMessage} </h2>
+        }
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {messageList : []}
+    }
+
+    renderResponse(message) {
+        this.setState(prevState => ({
+            messageList: [...prevState.messageList, message]
+        }))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Chatter onReceivedMessage={this.renderResponse.bind(this)}/>
+                <Responder willRenderMessage={this.state.messageList}/>
+            </div>
+        );
+    }
+}
+
 
 export default App;
